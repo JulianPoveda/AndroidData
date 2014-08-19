2aed47d0d754941bebfd698530fd62d958fb0ea8
lindasypelc <franktaguaros@gmail.com>
2014-08-19 08:04:28 -0500

    Validaciones_administrativas
    se valida el no ingreso de las observaciones administrativas, creando la
    clase validación y una función que valida cada observación.

--- src/class_amdata/Class_Impresiones.java
+++ src/class_amdata/Class_Impresiones.java
@@ -31,6 +31,7 @@ public class Class_Impresiones {
 	/**Funcion encargada de validar los datos antes de imprimir el acta de revision**/
 	public boolean validarDatosImpresionActa(String _ordenTrabajo){
 		boolean _retorno = true;
+		
 		/**validacion de los sellos**/
 		if(!this.ActasSQL.ExistRegistros("amd_sellos", "id_orden='"+_ordenTrabajo+"'")){
 			if(!this.ActasSQL.ExistRegistros("amd_inconsistencias", "id_orden='"+_ordenTrabajo+"' AND cod_inconsistencia='AD13'")){
@@ -38,8 +39,47 @@ public class Class_Impresiones {
 				_retorno = false;
 			}
 		}
+			/**validacion medidor instalado**/
+			if(!this.ActasSQL.ExistRegistros("amd_cambios_contadores", "id_orden='"+_ordenTrabajo+"'")){
+				if(!this.ActasSQL.ExistRegistros("amd_inconsistencias", "id_orden='"+_ordenTrabajo+"' AND cod_inconsistencia='AD15'")){
+					Toast.makeText(this._ctxActas,"No existen registro de instalacion de contador ni observacion administrativa AD15, no es posible imprimir.", Toast.LENGTH_SHORT).show();
+					_retorno = false;
+				}
+	
+			}
+			/**validacion acometida**/
+				if(!this.ActasSQL.ExistRegistros("amd_acometida", "id_orden='"+_ordenTrabajo+"'")){
+					if(!this.ActasSQL.ExistRegistros("amd_inconsistencias", "id_orden='"+_ordenTrabajo+"' AND cod_inconsistencia='AD10'")){
+						Toast.makeText(this._ctxActas,"No existen registro de acometida ni observacion administrativa AD10, no es posible imprimir.", Toast.LENGTH_SHORT).show();
+						_retorno = false;
+					}
+				}
+		/**validacion aforo**/
+					if(!this.ActasSQL.ExistRegistros("amd_censo_carga", "id_orden='"+_ordenTrabajo+"'")){
+						if(!this.ActasSQL.ExistRegistros("amd_inconsistencias", "id_orden='"+_ordenTrabajo+"' AND cod_inconsistencia='AD11'")){
+							Toast.makeText(this._ctxActas,"No existen registro de censo de carga ni observacion administrativa AD11, no es posible imprimir.", Toast.LENGTH_SHORT).show();
+							_retorno = false;
+						}
+					}
+		/**validacion porcentaje error**/
+		if(!this.ActasSQL.ExistRegistros("amd_pct_error", "id_orden='"+_ordenTrabajo+"'")){
+			if(!this.ActasSQL.ExistRegistros("amd_inconsistencias", "id_orden='"+_ordenTrabajo+"' AND cod_inconsistencia='AD12'")){
+				Toast.makeText(this._ctxActas,"No existen registro de Pct Error ni observacion administrativa AD12, no es posible imprimir.", Toast.LENGTH_SHORT).show();
+				_retorno = false;
+			}
+		}		
+
+	/**validacion IRREGULARIDADES**/
+	if(!this.ActasSQL.ExistRegistros("amd_irregularidades", "id_orden='"+_ordenTrabajo+"'")){
+		if(!this.ActasSQL.ExistRegistros("amd_inconsistencias", "id_orden='"+_ordenTrabajo+"' AND cod_inconsistencia='AD14'")){
+			Toast.makeText(this._ctxActas,"No existen registro de Irregularidades ni observacion administrativa AD14, no es posible imprimir.", Toast.LENGTH_SHORT).show();
+			_retorno = false;
+		}
+		
+	}		
 		
 		
 		return _retorno;		
 	}
+
 }
